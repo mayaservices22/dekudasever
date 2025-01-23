da5b5d52428788fa855fcd8f6a013f068b51d921
mayaservices22 <mayaservices84@gmail.com>
2025-01-23 13:49:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "makinmoni247@gmail.com";
 //const pass = "zusxabzqbuqlxyis";
-// 13 jan
-
+// 01 feb
 // Middleware
 app.use(express.json());
 
